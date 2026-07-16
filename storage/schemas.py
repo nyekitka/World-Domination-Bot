7686f66916057fff0ec1b91568093d9d30384900
nyekitka <nikita.klinov.2016@gmail.com>
2026-07-16 20:51:12 +0300

    Add storage method for extracting all needed info about orders. The method stores information in new class called OrderInfo

--- storage/schemas.py
+++ storage/schemas.py
@@ -15,6 +15,17 @@ class MessageType(StrEnum):
     NEGOTIATIONS = auto()
     ATTACK = auto()
 
+
+class OrderInfo(BaseModel):
+    shielded: list[int]
+    developed: list[int]
+    sanctions: list[int]
+    created: int
+    is_invented: bool
+    eco_boost: bool
+    attacked: list[int]
+
+
 INFO_MESSAGE_TYPES = (
     MessageType.CITY,
     MessageType.METEORITES,
